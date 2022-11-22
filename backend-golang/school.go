d10d4fa4b18d448c5d2ec27aa4b0c36b50396c6a
MichailFerapontov <mferapon@gmail.com>
2022-11-23 00:20:42 +0300

    Changed Guardian struct

--- backend-golang/school.go
+++ backend-golang/school.go
@@ -1,10 +1,10 @@
 package school
 
 type Guardian struct {
-	Id      int    `json:"guadian_id" db:"guadian_id"`
-	Name    string `json:"name" db:"name"`
-	Surname string `json:"surname" db:"surname"`
-	Gender  string `json:"gender" db:"gender"`
-	Phone   string `json:"phone" db:"phone"`
-	Email   string `json:"email" db:"email"`
+	Guardian_id int    `json:"guadian_id"`
+	Name        string `json:"name" db:"name"`
+	Surname     string `json:"surname" db:"surname"`
+	Gender      string `json:"gender" db:"gender"`
+	Phone       string `json:"phone" db:"phone"`
+	Email       string `json:"email" db:"email"`
 }
